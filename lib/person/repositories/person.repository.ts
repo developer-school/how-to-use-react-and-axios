6bee1154167dc46c8ff1bd653005d496f1d7a56e
Paul Halliday <hello@paulhalliday.io>
2021-11-03 12:01:01 +0000

    Added typings

--- lib/person/repositories/person.repository.ts
+++ lib/person/repositories/person.repository.ts
@@ -1,7 +1,16 @@
 import { AxiosInstance } from "axios";
 import axiosInstance from "../../http/client/axios.client";
+import { Person } from "../models/person";
 
-export function PersonRepository(axios: AxiosInstance) {
+type IPersonRepository = {
+  getAll: () => Promise<Person[]>;
+  getById: (id: number) => Promise<Person>;
+  getByName: (name: string) => Promise<Person>;
+  getByEmail: (email: string) => Promise<Person>;
+  create: (person: Person) => Promise<Person>;
+};
+
+export function PersonRepository(axios: AxiosInstance): IPersonRepository {
   return {
     getAll: async () => (await axios.get("/people")).data,
     getById: async (id: number) => (await axios.get(`/people/${id}`)).data,
@@ -9,6 +18,8 @@ export function PersonRepository(axios: AxiosInstance) {
       (await axios.get(`/people?name=${name}`)).data,
     getByEmail: async (email: string) =>
       (await axios.get(`/people?email=${email}`)).data,
+    create: async (person: Person) =>
+      (await axios.post("/people", person)).data,
   };
 }
 
