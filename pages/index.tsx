24257a2ae04525cce4531677107300e3d68e5b8a
Paul Halliday <hello@paulhalliday.io>
2021-11-03 13:46:56 +0000

    Fixed stroke

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,9 +43,9 @@ export default function Home() {
             aria-hidden='true'
           >
             <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
               d='M12 6v6m0 0v6m0-6h6m-6 0H6'
             />
           </svg>
